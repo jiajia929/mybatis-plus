d3d3cbc3a26b926710ce8087da9b97c3da95f2e0
panjiasheng <1543139916@qq.com>
2023-07-31 16:30:46 +0800

    mybatis-plus-乐观锁

--- src/main/java/com/example/mybatisplus/config/MyBatisPlusConfig.java
+++ src/main/java/com/example/mybatisplus/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.example.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +10,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 //扫描mapper接口所在的包
-@MapperScan("com.example.mybatisplus.mapper")
+//@MapperScan("com.example.mybatisplus.mapper")
 public class MyBatisPlusConfig {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //添加分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //添加乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
--- src/test/java/com/example/mybatisplus/MyBatisPlusPlugins.java
+++ src/test/java/com/example/mybatisplus/MyBatisPlusPlugins.java
@@ -1,7 +1,9 @@
 package com.example.mybatisplus;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mybatisplus.mapper.ProductMapper;
 import com.example.mybatisplus.mapper.UserMapper;
+import com.example.mybatisplus.pojo.Product;
 import com.example.mybatisplus.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ public class MyBatisPlusPlugins {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private ProductMapper productMapper;
 
     @Test
     public void testPage() {
@@ -33,5 +38,30 @@ public class MyBatisPlusPlugins {
         System.out.println(page.getTotal());
         System.out.println(page.hasNext());
         System.out.println(page.hasPrevious());
+    }
+
+    @Test
+    public void testProduct01() {
+        //小李查询商品价格
+        Product productLi = productMapper.selectById(1);
+        System.out.println("小李查询的商品价格：" + productLi.getPrice());
+        //小王查询商品价格
+        Product productWang = productMapper.selectById(1);
+        System.out.println("小王查询的商品价格：" + productWang.getPrice());
+        //小李将商品价格+50
+        productLi.setPrice(productLi.getPrice() + 50);
+        productMapper.updateById(productLi);
+        //小王将商品价格-30
+        productWang.setPrice(productWang.getPrice() - 30);
+        int result = productMapper.updateById(productWang);
+        if (result == 0) {
+            //操作失败，重试
+            Product productNew = productMapper.selectById(1);
+            productNew.setPrice(productNew.getPrice()-30);
+            productMapper.updateById(productNew);
+        }
+        //老板查询商品价格
+        Product productLaoban = productMapper.selectById(1);
+        System.out.println("老板查询的商品价格：" + productLaoban.getPrice());
     }
 }
